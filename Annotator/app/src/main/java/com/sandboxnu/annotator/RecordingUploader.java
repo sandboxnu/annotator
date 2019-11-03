bd997e707e61caec2b93666aad33b76a94f33d86
jakechvatal <jakechvatal@github.com>
2019-11-03 11:20:16 -0500

    added framework to android app

--- Annotator/app/src/main/java/com/sandboxnu/annotator/RecordingUploader.java
+++ Annotator/app/src/main/java/com/sandboxnu/annotator/RecordingUploader.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,12 +19,80 @@ import android.widget.Toast;
 
 public class RecordingUploader {
 
-    String UPLOAD_ENDPOINT = "";
+    private String UPLOAD_ENDPOINT = "";
 
     public void uploadAll() {
+        try {
+            // TODO: this is bad practice, remove later
+            java.io.File test = new java.io.File("test.txt");
+            File folder = test.getParentFile();
+            File[] listOfFiles = folder.listFiles();
 
+            if(listOfFiles == null) {
+                // TODO: produce a warning or error here.
+
+            } else {
+                for(File file :  listOfFiles) {
+                    File encrypted = this.encryptFile(file);
+                    this.uploadFile(encrypted);
+                    this.removeFile(encrypted);
+                }
+            }
+
+        }catch(Exception e) {
+            // TODO: catch exception in gui
+            // throw new IllegalAccessException("Unable to access the files in the directory.");
+        }
     }
-/*
+
+    /**
+     * Encrypts a file and returns its encrypted contents.
+     * @param file the File to be encrypted.
+     * @return the contents of the File, encrypted.
+     */
+    private String encryptFileString(File file) {
+        String fileStr = this.parseFile(file);
+        String pub_key = "";
+
+        return "";
+        // TODO: is this necessary?
+    }
+
+    /**
+     * Replaces a file with an encrypted copy.
+     * @param file the File to be encrypted.
+     * @return the newly encrypted File.
+     */
+    private File encryptFile(File file) {
+        // TODO: this method
+        return file;
+    }
+
+    private void removeFile(File file) {
+        boolean success = file.delete();
+        // TODO: assert that the file removal was a success in another way
+        assert success;
+    }
+
+    /**
+     * Parses the given file into its String represeentation.
+     * @return the String representation of the encrypted file.
+     */
+    private String parseFile(File file) {
+         try {
+             FileInputStream fis = new FileInputStream(file);
+
+            byte[] data = new byte[(int) file.length()];
+            fis.read(data);
+            fis.close();
+
+            return new String(data, "UTF-8");
+         } catch(Exception e) {
+             // TODO: catch this exception
+             // throw new IllegalAccessException("The file cannot be read.");
+         }
+    }
+
     private void uploadFile(File file) {
         HttpURLConnection conn = null;
         DataOutputStream dos = null;
@@ -33,7 +102,6 @@ public class RecordingUploader {
         int bytesRead, bytesAvailable, bufferSize;
         byte[] buffer;
         int maxBufferSize = 1 * 1024 * 1024;
-
 
         try {
             FileInputStream fileInputStream = new FileInputStream(file);
@@ -53,13 +121,13 @@ public class RecordingUploader {
 
             dos.writeBytes(twoHyphens + boundary + lineEnd);
             dos.writeBytes("Content-Disposition: form-data; name="uploaded_file";filename=""
-                            + fileName + """ + lineEnd);
+                            + fileName + "" + lineEnd);
 
                     dos.writeBytes(lineEnd);
         }
         catch(Exception e) {
 
         }
-    }*/
+    }
 
 }
