cca294094ff81e5e7d62013d3a36bb3f7814c3f2
jakechvatal <jakechvatal@github.com>
2019-11-17 14:10:38 -0500

    initial s3 upload work

--- Annotator/app/src/main/java/com/sandboxnu/annotator/MainActivity.java
+++ Annotator/app/src/main/java/com/sandboxnu/annotator/MainActivity.java
@@ -13,9 +13,11 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.ToggleButton;
+
 
 import java.io.File;
 
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         final Intent alarmIntent = new Intent(MainActivity.this, WakeableService.class);
         ToggleButton toggle = findViewById(R.id.fab);
@@ -42,15 +45,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        File recordingsFolder = this.getFilesDir();
+        final File recordingsFolder = this.getFilesDir();
         File[] recordings = recordingsFolder.listFiles();
         String[] fileNames = new String[recordings.length];
+
         for(int i=0; i<recordings.length; i++) {
             fileNames[i] = recordings[i].getName();
         }
+
+        final MainActivity ref = this;
+
         ListView recordingsView = findViewById(R.id.recordings);
         ArrayAdapter arrayAdapter = new ArrayAdapter<>(this, R.layout.activity_listview, fileNames);
         recordingsView.setAdapter(arrayAdapter);
+
+        Button upload = findViewById(R.id.upload);
+        upload.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new RecordingUploader(ref).uploadAll(recordingsFolder);
+            }
+        });
     }
 
     @Override
@@ -75,3 +89,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
